acc4ff3d923fea63533a10cff42934670e27a562
Raghav Kumar <jharaghav330@gmail.com>
2025-01-12 00:21:06 +0530

    initialize react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-3xl'>hello</h1>
+    </div>
+  )
+
+
+}
+
+export default App
